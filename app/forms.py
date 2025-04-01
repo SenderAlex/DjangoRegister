c857b0f81a9a9855857849c08bbe13cafb965be5
Alexander Sender <141014024+SenderAlex@users.noreply.github.com>
2025-04-02 02:05:30 +0300

    finished

--- app/forms.py
+++ app/forms.py
@@ -7,9 +7,3 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'phone_number', 'password1', 'password2')  # Добавьте необходимые поля
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if CustomUser.objects.filter(username=username).exists():
-            raise ValidationError("Пользователь с таким именем уже существует.")
-        return username
